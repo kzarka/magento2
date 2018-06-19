0e2727706df3d5719e79c003236f6af4baab5a21
Huy Tien <nhtienptit@gmail.com>
2018-06-19 18:40:43 +0700

    Session practice

--- app/code/OpenCert/Hello/Block/HelloWorld.php
+++ app/code/OpenCert/Hello/Block/HelloWorld.php
@@ -5,12 +5,16 @@ class Helloworld extends \Magento\Framework\View\Element\Template
 {
 
 	protected $_registry;
+	protected $_catalogSession;
+
 	public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Framework\Registry $registry,
+        \Magento\Catalog\Model\Session $catalogSession,
         array $data = []
     ) {
         $this->_registry = $registry;
+        $this->_catalogSession = $catalogSession;
         parent::__construct($context, $data);
    	}
 
@@ -19,4 +23,10 @@ class Helloworld extends \Magento\Framework\View\Element\Template
         return 'Hello world!'.$this->_registry->registry('custom_var');
 
     }
+
+    public function getCatalogSession() 
+    {
+        return $this->_catalogSession;
+    }
+
 }
