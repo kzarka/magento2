92519ec007991f2f67771a8b1dc9c98bc677ecd0
Huy Tien <nhtienptit@gmail.com>
2018-06-19 18:28:53 +0700

    Registry practice

--- app/code/OpenCert/Hello/Block/HelloWorld.php
+++ app/code/OpenCert/Hello/Block/HelloWorld.php
@@ -1,10 +1,22 @@
 <?php
 namespace OpenCert\Hello\Block;
- 
+//use OpenCert\Hello\Controller\Index\Index;
 class Helloworld extends \Magento\Framework\View\Element\Template
 {
+
+	protected $_registry;
+	public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+   	}
+
     public function getHelloWorldTxt()
     {
-        return 'Hello world!';
+        return 'Hello world!'.$this->_registry->registry('custom_var');
+
     }
 }
