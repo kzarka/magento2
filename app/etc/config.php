12e244c67e20e2cdbb0e329e6189c81d313ed7b3
Huy Tien <nhtienptit@gmail.com>
2018-06-19 16:21:04 +0700

    Nop bai hello world

--- app/etc/config.php
+++ app/etc/config.php
@@ -144,6 +144,7 @@ return [
     'Magento_WishlistAnalytics' => 1,
     'Magento_WishlistSampleData' => 1,
     'Mgt_DeveloperToolbar' => 1,
+    'OpenCert_Hello' => 1,
     'Shopial_Facebook' => 1,
     'Temando_Shipping' => 1,
     'Vertex_Tax' => 1
