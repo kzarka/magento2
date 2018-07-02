cf65e7dbed07d431363955a8b4c0a79179ddba65
Huy Tien <nhtienptit@gmail.com>
2018-07-02 17:50:32 +0700

    Observer Discount Update Event

--- app/code/Task4/ObserverDiscount/Observer/Discount.php
+++ app/code/Task4/ObserverDiscount/Observer/Discount.php
@@ -30,16 +30,5 @@ class Discount implements ObserverInterface
 
             return $this;
         }
-        else echo "Not logged!";
-        die();
-        
-        /*$item = $observer->getEvent()->getData('quote_item');
-        $item = ($item->getParentItem() ? $item->getParentItem() : $item);
-        $price = $item->getProduct()->getPriceInfo()->getPrice('final_price')->getValue();
-        $new_price = $price - ($price * 50 / 100); //discount the price of the product to 50%
-        $item->setCustomPrice($new_price);
-        $item->setOriginalCustomPrice($new_price);
-        $item->getProduct()->setIsSuperMode(true);
-        */
     }
 }
