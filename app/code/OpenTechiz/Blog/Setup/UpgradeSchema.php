a8ea51aec3c732737a54d36cdd97b61d87e8c335
Huy Tien <nhtienptit@gmail.com>
2018-07-10 16:44:45 +0700

    Create Table Notification Comment Approval

--- app/code/OpenTechiz/Blog/Setup/UpgradeSchema.php
+++ app/code/OpenTechiz/Blog/Setup/UpgradeSchema.php
@@ -13,17 +13,32 @@ class UpgradeSchema implements UpgradeSchemaInterface
 	{
         $installer = $setup;
 
-    	$installer->startSetup();
-    	if (version_compare($context->getVersion(), "1.2.5", "<")) {
-		    $tableName = $installer->getTable('opentechiz_blog_comment');
-            $installer->getConnection()->addColumn($tableName, 'email', [
-                'type' => Table::TYPE_TEXT,
-                'length' => 100,
-                'nullable' => false,
-                'comment' => 'Email'
-            ]);
-		}
+        $installer->startSetup();
+        // create notification table
+        $table = $installer->getConnection()
+            ->newTable($installer->getTable('opentechiz_blog_comment_approval_notification'))
+            ->addColumn('noti_id', Table::TYPE_SMALLINT, null, [
+               'identity' => true,
+               'nullable' => false,
+               'primary' => true,
+            ], 'Noti ID')
+            ->addColumn('content', Table::TYPE_TEXT, 255, ['nullable => false'], 'Notification Content')
+            ->addColumn('user_id', Table::TYPE_SMALLINT, null, ['nullable' => false], 'User ID')
+            ->addColumn('post_id', Table::TYPE_SMALLINT, null, ['nullable' => false], 'Post ID')
+            ->addColumn('creation_time', Table::TYPE_TIMESTAMP, null, [], 'Comment Created At')
+            ->setComment('Comment Notification');
 
-    	$installer->endSetup();
+        $installer->getConnection()->createTable($table);
+        
+        // modified comment table
+        $tableName = $installer->getTable('opentechiz_blog_comment');
+            $installer->getConnection()->addColumn($tableName, 'user_id', [
+                'type' => Table::TYPE_SMALLINT,
+                'nullable' => true,
+                'default' => NULL,
+                'comment' => 'User ID'
+            ]);
+
+        $installer->endSetup();
     }
 }
